a2c96925d6680f2341f0016d2fa843f21b5af0fc
RifatRobin <rifatitna@gmail.com>
2021-02-06 20:45:42 +0600

    Updated

--- footballer_state.py
+++ footballer_state.py
@@ -1,31 +1,64 @@
 import streamlit as st
 import pandas as pd
-import matplotlib as plt
-import seaborn as sb
-import numpy as np
 import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-st.title("Footballer States")
+st.title('NFL Football Stats (Rushing) Explorer')
 
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1920,2021))))
 
-st.sidebar.header("Input Section")  # for side bar title
-selected_year = st.sidebar.selectbox("Year", list(
-    reversed(range(1920, 2021))))  # input of year in sidebar
-
-# web scrapping for the footballer state data
-
+# Web scraping of NFL player stats
+# https://www.pro-football-reference.com/years/2020/rushing.htm
 
 @st.cache
 def load_data(year):
-    url = 'https://www.pro-football-reference.com/years/' + \
-        str(year) + "/rushing.htm"
-    read_data = pd.read_html(url, header=1)
-    df = read_data[0]
-    # Deletes repeating headers in content
-    raw = df.drop(df[df.Age == 'Age'].index)
+    url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
+    html = pd.read_html(url, header = 1)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
-
-
 playerstats = load_data(selected_year)
+
+# Sidebar - Team selection
+sorted_unique_team = sorted(playerstats.Tm.unique())
+selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+
+# Sidebar - Position selection
+unique_pos = ['RB','QB','WR','FB','TE']
+selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+
+# Filtering data
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
+
+# Download NBA player stats data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+
+# Heatmap
+if st.button('Intercorrelation Heatmap'):
+    st.header('Intercorrelation Matrix Heatmap')
+    df_selected_team.to_csv('output.csv',index=False)
+    df = pd.read_csv('output.csv')
+
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+    st.pyplot()
